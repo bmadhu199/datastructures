1738fa6c633db900e2a097cbf810bdc586d962a2
Madhusudhan <madhu.sudhan@abcplusd.net>
2018-04-27 18:23:51 +0530

    adding some n^2 complexity sortings

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,61 @@
+package dataStructures.sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+  
+  int[] bubbleSorting(int[] arr, int length){
+    
+    
+    for(int i = 0; i<length;i++)
+    {
+      for(int j =0 ; j< length-i-1;j++)
+      {
+        
+        if(arr[j] > arr[j+1])
+        {
+          int temp = arr[j+1];
+          arr[j+1] = arr[j];
+          arr[j] = temp;
+        }
+        
+        
+      }
+    }
+    
+    
+    return arr;
+    
+  }
+  
+  
+
+  public static void main(String[] args) {
+
+Scanner scr = new Scanner(System.in);
+    
+    System.out.println("Please enter array lenght ");
+    int length =  scr.nextInt();
+    
+    int[] arr = new int[length];
+    
+    System.out.println("Please enter set of number to sort...! ");
+    
+    for(int i = 0;i<length;i++)
+    {
+        arr[i] = scr.nextInt();
+    }
+    
+    BubbleSort bubSort = new BubbleSort();
+    
+    bubSort.bubbleSorting(arr, length);
+    
+    for(int i = 0;i<length;i++)
+    {
+      System.out.println(arr[i]);
+    }
+    
+    
+  }
+
+}
---
+++ sorting/InsertionSort.java
@@ -0,0 +1,61 @@
+package dataStructures.sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+  
+  
+  
+  int[] InsertionSorting(int[] arr,int length){
+    
+    
+    for(int i = 1;i<length;i++)
+    {
+       int key = arr[i];
+       
+       int j = i-1;
+       
+       while(j>=0 && key < arr[j])
+       {
+          arr[j+1] = arr[j];
+         
+          j = j-1;
+       }
+       
+       arr[j+1] =key;
+    }
+    
+    
+   return arr; 
+    
+  }
+
+  public static void main(String[] args) {
+
+Scanner scr = new Scanner(System.in);
+    
+    System.out.println("Please enter array lenght ");
+    int length =  scr.nextInt();
+    
+    int[] arr = new int[length];
+    
+    System.out.println("Please enter set of number to sort...! ");
+    
+    for(int i = 0;i<length;i++)
+    {
+        arr[i] = scr.nextInt();
+    }
+    
+    
+    InsertionSort inrSort = new InsertionSort();
+    
+    inrSort.InsertionSorting(arr, length);
+    
+    for(int i =0 ;i<length ;i++)
+    {
+      System.out.println(arr[i]);
+    }
+    
+  }
+
+}
